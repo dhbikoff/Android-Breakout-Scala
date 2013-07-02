8068a82bd9b4f616b0e3e347bede1d950e576fca
dhbikoff <dhbikoff@gmail.com>
2013-07-02 12:36:57 -0400

    Refactored Splash

--- src/com/dhbikoff/breakout/Splash.scala
+++ src/com/dhbikoff/breakout/Splash.scala
@@ -6,108 +6,105 @@ import java.io.IOException
 import java.io.ObjectInputStream
 import java.io.StreamCorruptedException
 
-import scala.collection.mutable.StringBuilder
-
 import android.app.Activity
 import android.content.Intent
-import android.content.SharedPreferences
 import android.media.AudioManager
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import android.widget.ToggleButton
-import android.util.Log
 
 class Splash extends Activity {
-  val FilePath = "data/data/com.dhbikoff.breakout/data.dat"
-  val NewGame = "NEW_GAME"
-  val HighScorePref = "HIGH_SCORE_PREF"
-  val SoundOnOff = "SOUND_ON_OFF"
-  val SoundPrefs = "SOUND_PREFS"
-  val ScoreStr = "High Score = "
-
-  var newGameVal = 0
-  var highScore = 0
-  var sound = true
-
-  override def onCreate(savedInstanceState: Bundle) = {
+  private val FilePath = "data/data/com.dhbikoff.breakout/data.dat"
+  private val NewGame = "NEW_GAME"
+  private val HighScorePref = "HIGH_SCORE_PREF"
+  private val SoundOnOff = "SOUND_ON_OFF"
+  private val SoundPrefs = "SOUND_PREFS"
+  private val ScoreStr = "High Score = "
+  private lazy val intent = new Intent(this, classOf[Breakout])
+  
+  override protected def onCreate(savedInstanceState: Bundle): Unit = {
     super.onCreate(savedInstanceState)
     setVolumeControlStream(AudioManager.STREAM_MUSIC)
     setContentView(R.layout.splash)
   }
 
-  def fetchHighScore: Int = {
-    var points = 0
+  private def currentScore = {
+    val scoreSettings = getSharedPreferences(HighScorePref, 0)
+	scoreSettings.getInt("highScore", 0)
+  }
+  
+  private def highScore: Int = {
+    var oldScore = 0
     try {
       val fis = new FileInputStream(FilePath)
       val ois = new ObjectInputStream(fis)
-      points = ois.readInt
+      oldScore = ois.readInt
       fis.close
     } catch {
       case e: FileNotFoundException => e.printStackTrace
       case e: StreamCorruptedException => e.printStackTrace
       case e: IOException => e.printStackTrace
 	}
-    points
+    if (oldScore > currentScore) oldScore 
+    else currentScore
   }
 
-  def showHighScore = {
-    val points = fetchHighScore
-    if (points > highScore)
-      highScore = points
-
-    val hiScore = (findViewById(R.id.hiScoreView)).asInstanceOf[TextView]
-    hiScore.setText(ScoreStr + highScore)
+  private def showHighScore() = {
+    val highScoreView = findViewById(R.id.hiScoreView).asInstanceOf[TextView]
+    highScoreView.setText(ScoreStr + highScore)
   }
 
-  def newGame(view: View) = {
-    newGameVal = 1
-    val intent = new Intent(this, classOf[Breakout])
-    intent.putExtra(NewGame, newGameVal)
-    intent.putExtra(SoundOnOff, sound)
+  def newGame(view: View): Unit = {  
+    intent.putExtra(NewGame, 1)
+    intent.putExtra(SoundOnOff, currentSound)
     startActivity(intent)
   }
 
-  def contGame(view: View) {
-    newGameVal = 0
-    val intent = new Intent(this, classOf[Breakout]);
-    intent.putExtra(NewGame, newGameVal);
-    intent.putExtra(SoundOnOff, sound);
-    startActivity(intent);
+  def contGame(view: View): Unit = {
+    intent.putExtra(NewGame, 0)
+    intent.putExtra(SoundOnOff, currentSound)
+    startActivity(intent)
   }
 
-  
-  override def onResume = {
-    super.onResume
+  private def savedSound: Boolean = {
     val soundSettings = getSharedPreferences(SoundPrefs, 0)
-	sound = soundSettings.getBoolean("soundOn", true)
-	val scoreSettings = getSharedPreferences(HighScorePref, 0)
-	highScore = scoreSettings.getInt("highScore", 0)
+	soundSettings.getBoolean("soundOn", true)
+  }
+  
+  private def currentSound: Boolean = {
+    val soundButton = (findViewById(R.id.soundToggleButton)).asInstanceOf[ToggleButton]
+    soundToggle(soundButton)
+  }
+  
+  def soundToggle(v: View): Boolean = 
+    v.asInstanceOf[ToggleButton] isChecked
+    
+  override protected def onResume: Unit = {
+    super.onResume
 	val soundButton = (findViewById(R.id.soundToggleButton)).asInstanceOf[ToggleButton]
-	soundButton.setChecked(sound)
-	showHighScore
+	soundButton.setChecked(savedSound)
+	showHighScore()
+  }
+  
+  def showSource(v: View): Unit = {
+    val link = new Intent
+    link.setAction(Intent.ACTION_VIEW)
+    link.addCategory(Intent.CATEGORY_BROWSABLE)
+    link.setData(Uri.parse("https://github.com/dhbikoff/AndroidBreakoutScala"))
+    startActivity(link)
   }
 
-  def soundToggle(v: View) = (v.asInstanceOf[ToggleButton]).isChecked
-
-  def showSource(v: View) = {
-    val intent = new Intent
-    intent.setAction(Intent.ACTION_VIEW)
-    intent.addCategory(Intent.CATEGORY_BROWSABLE)
-    intent.setData(Uri.parse("http://github.com/dhbikoff/Android-Breakout"))
-    startActivity(intent)
-  }
-
-  override def onPause = {
+  override protected def onPause: Unit = {
     super.onPause
-    val soundSettings = getSharedPreferences(SoundPrefs, 0);
-    val highScoreSave = getSharedPreferences(HighScorePref,0);
-    val soundEditor = soundSettings.edit();
-    val scoreEditor = highScoreSave.edit();
-    scoreEditor.putInt("highScore", highScore);
-    soundEditor.putBoolean("soundOn", sound);
-    soundEditor.commit();
-    scoreEditor.commit();
+    val soundSettings = getSharedPreferences(SoundPrefs, 0)
+    val highScoreSave = getSharedPreferences(HighScorePref,0)
+    val soundEditor = soundSettings.edit()
+    val scoreEditor = highScoreSave.edit()
+    scoreEditor.putInt("highScore", highScore)
+    soundEditor.putBoolean("soundOn", currentSound)
+    soundEditor.commit()
+    scoreEditor.commit()
   }
 }
